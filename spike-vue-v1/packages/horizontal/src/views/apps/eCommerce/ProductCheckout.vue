671d23a7c559b22294b7435579ad1b11fbcad33a
webfazliddin <webfazliddin@gmail.com>
2025-03-26 10:37:53 +0500

    change

--- spike-vue-v1/packages/horizontal/src/views/apps/eCommerce/ProductCheckout.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import CartCheckout from '@/components/apps/ecommerce/cart/CartCheckout.vue';
-</script>
-
-<template>
-    <CartCheckout />
-</template>
--- spike-vue-v1/packages/minisidebar/src/views/apps/kanban/Kanban.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import {ref} from 'vue'
-import TaskCard from "@/components/apps/kanban/TaskCard.vue";
- </script>
-
-<template>
-    <TaskCard/> 
-</template>
-
